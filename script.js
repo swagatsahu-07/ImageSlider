d6999004fe338618adf2386a184eb7b3b63433c0
swagatsahu-07 <swagatsahu9090@gmail.com>
2024-11-17 21:25:40 +0530

    done

---
+++ script.js
@@ -0,0 +1,48 @@
+let nextBtn = document.querySelector('.next');
+let prevBtn = document.querySelector('.prev');
+
+let slider = document.querySelector('.slider');
+let sliderList = slider.querySelector('.slider .list');
+let thumbnail = document.querySelector('.slider .thumbnail');
+let thumbnailItems = thumbnail.querySelectorAll('.item');
+
+thumbnail.appendChild(thumbnailItems[0]);
+
+// Add a variable to store the direction
+let currentDirection = "";
+
+// Function for next button
+nextBtn.onclick = function() {
+    moveSlider('next');
+}
+
+// Function for prev button
+prevBtn.onclick = function() {
+    moveSlider('prev');
+}
+
+function moveSlider(direction) {
+    let sliderItems = sliderList.querySelectorAll('.item');
+    let thumbnailItems = document.querySelectorAll('.thumbnail .item');
+   
+    currentDirection = direction; // Update direction globally
+    
+    if (direction === 'next') {
+        sliderList.appendChild(sliderItems[0]);
+        thumbnail.appendChild(thumbnailItems[0]);
+        slider.classList.add('next');
+    } else {
+        sliderList.prepend(sliderItems[sliderItems.length - 1]);
+        thumbnail.prepend(thumbnailItems[thumbnailItems.length - 1]);
+        slider.classList.add('prev');
+    }
+}
+
+slider.addEventListener('animationend', function() {
+    // Use the global currentDirection variable
+    if (currentDirection === 'next') {
+        slider.classList.remove('next');
+    } else if (currentDirection === 'prev') {
+        slider.classList.remove('prev');
+    }
+}, { once: false }); // Allow the event listener to run multiple times
